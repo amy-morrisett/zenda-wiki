30bdfdc02848f1fb933d4ff57d79dca489e5aea9
Amy Morrisett <morrisettamy@gmail.com>
2023-01-13 14:51:33 -0500

    cleaned up commented out code in RouteList

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { db } from './firebase';
-// import { getDoc, updateDoc, doc } from 'firebase/firestore';
+import { db } from './firebase';
+import { setDoc, doc } from 'firebase/firestore';
 
 function Home() {
+  const [articleName, setArticleName] = useState('');
+  const [articleText, setArticleText] = useState('');
+  const [articleTags, setArticleTags] = useState('');
+
+  function handleArticleName(evt) {
+    if (evt.target.value) {
+      setArticleName(evt.target.value);
+    }
+  }
+  function handleArticleText(evt) {
+    if (evt.target.value) {
+      setArticleText(evt.target.value);
+    }
+  }
+  function handleArticleTags(evt) {
+    if (evt.target.value) {
+      setArticleTags(evt.target.value);
+    }
+  }
+  function handleClick() {
+    const articleDoc = doc(db, 'articles', articleName);
+    setDoc(articleDoc, { text: articleText, tags: articleTags.split(' ') });
+  }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
   return (
     <div className="Home">
       <p>Welcome to the first ever Zenda Wiki!</p>
       <Link to="/categories">Categories</Link>
+      <p>Add a new article here!</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="title"
+            value={articleName}
+            onChange={handleArticleName}
+          />
+        </div>
+        <div>
+          <input
+            type="textarea"
+            placeholder="text"
+            value={articleText}
+            onChange={handleArticleText}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="tags -- separate with a space"
+            value={articleTags}
+            onChange={handleArticleTags}
+          />
+        </div>
+        <button type="submit" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/RouteList.js
+++ src/RouteList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { db } from './firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 import Home from './Home';
 
@@ -10,33 +10,13 @@ import Categories from './Categories';
 import Gifts from './categories/Gifts';
 import Characters from './categories/Characters';
 
-// import Kani from './categories/gifts/Kani';
-// import Enti from './categories/gifts/Enti';
-// import AuraSight from './categories/gifts/AuraSight';
-// import EmotionPainting from './categories/gifts/EmotionPainting';
-// import IcePowers from './categories/gifts/IcePowers';
-// import ImageSinging from './categories/gifts/ImageSinging';
-// import Koah from './categories/gifts/Koah';
-// import Levitation from './categories/gifts/Levitation';
-// import PsychicPowers from './categories/gifts/PsychicPowers';
-// import SoundPainting from './categories/gifts/SoundPainting';
-// import Teaching from './categories/gifts/Teaching';
-
-import Zenda from './categories/characters/Zenda';
-import AlexandraWhite from './categories/characters/AlexandraWhite';
-
 import ArticleTemplate from './ArticleTemplate';
 
 function RouteList() {
-  //const kani = 'kani';
   const [articleArr, setArticleArr] = useState([]);
 
   useEffect(() => {
     const articlesRef = collection(db, 'articles');
-    // const giftQuery = query(
-    //   articlesRef,
-    //   where('tags', 'array-contains', 'gift')
-    // );
     const getArticleArr = async () => {
       const articleRefSnap = await getDocs(articlesRef);
       let allArticles = [];
@@ -62,17 +42,6 @@ function RouteList() {
 
         <Route path="/gifts" element={<Gifts />} />
         <Route path="/characters" element={<Characters />} />
-        {/* <Route path="/gifts/kani" element={<Kani />} /> */}
-        {/* <Route path="/gifts/enti" element={<Enti />} />
-        <Route path="/gifts/aura-sight" element={<AuraSight />} />
-        <Route path="/gifts/emotion-painting" element={<EmotionPainting />} />
-        <Route path="/gifts/ice-powers" element={<IcePowers />} />
-        <Route path="/gifts/image-singing" element={<ImageSinging />} />
-        <Route path="/gifts/koah" element={<Koah />} />
-        <Route path="/gifts/levitation" element={<Levitation />} />
-        <Route path="/gifts/psychic-powers" element={<PsychicPowers />} />
-        <Route path="/gifts/sound-painting" element={<SoundPainting />} />
-        <Route path="/gifts/teaching" element={<Teaching />} /> */}
 
         {articleArr.map((articleName) => (
           <Route
@@ -82,14 +51,6 @@ function RouteList() {
             }
           />
         ))}
-
-        {/* <Route path="/gifts/test" element={<ArticleTemplate article={kani} />} /> */}
-
-        <Route path="/characters/zenda" element={<Zenda />} />
-        <Route
-          path="/characters/alexandra-white"
-          element={<AlexandraWhite />}
-        />
       </Routes>
     </div>
   );
